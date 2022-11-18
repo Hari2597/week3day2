58960519b09b491d0d470854d70f275a0f3752e9
Hari2597 <117000965+Hari2597@users.noreply.github.com>
2022-11-18 14:43:14 +0530

    Add files via upload

---
+++ week3day2/FindSecondLargest.java
@@ -0,0 +1,23 @@
+package week3day2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+public class FindSecondLargest {
+public static void main(String[] args) {
+		Integer[] data= {3,2,11,4,6,7};
+		List<Integer>lar=new ArrayList<Integer>(Arrays.asList(data));
+		Collections.sort(lar);
+		for (int i = 0; i < lar.size(); i++) {
+			int num=lar.get(i);
+			{
+				
+			}
+			}
+			System.out.println(lar);
+			Collections.sort(lar);
+			System.out.println("second Largest" + " "+lar.get(4));
+		}
+}
---
+++ week3day2/RemoveDuplicateString.java
@@ -0,0 +1,22 @@
+package week3day2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+public class RemoveDuplicateString {
+public static void main(String[] args) {
+		String text ="We learn java basics as part of java sessions in java week1";
+		System.out.println(text);
+		String[] split=text.split(" ");
+		int length = split.length;
+		System.out.println(length);
+		Set<String>set=new LinkedHashSet<String>();
+		for (int i = 0; i < length; i++) {
+			set.add(split[i]);
+		}
+	    System.out.println(set);
+}
+	}
